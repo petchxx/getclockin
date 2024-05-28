70648b2ae253429d1e040e0b8ab0f9f1e86333db
Petchx <petchxpanuphong@gmail.com>
2024-05-28 17:42:12 +0700

    feat: remove unused code

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -10,12 +10,6 @@ import DiscordProvider from "next-auth/providers/discord";
 
 import { env } from "~/env";
 import { db } from "~/server/db";
-import {
-  accounts,
-  sessions,
-  users,
-  verificationTokens,
-} from "~/server/db/schema";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
