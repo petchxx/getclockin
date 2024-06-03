14528df27f047f0d6bb7fcf9473683d99615d957
Petchx <petchxpanuphong@gmail.com>
2024-06-03 20:03:32 +0700

    fixL remove employee api

---
+++ src/lib/interface/employee.ts
@@ -0,0 +1,10 @@
+export interface Employee {
+  id: string;
+  email: string;
+  status: string;
+  name: string | null;
+  start_time: string | null;
+  stop_time: string | null;
+  role: string | null;
+  salary: number | null;
+}
