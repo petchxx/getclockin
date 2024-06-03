c5d4950f8c379086cf33834b2051bbc119eecabd
Petchx <petchxpanuphong@gmail.com>
2024-06-03 08:08:03 +0700

    fix: company type

--- src/lib/interface/company.ts
+++ src/lib/interface/company.ts
@@ -2,7 +2,6 @@ export interface Company {
   id: string;
   email: string;
   status: string;
-  company_key: string | null;
   name: string | null;
   line_token: string | null;
   stripe_customer_id: string | null;
