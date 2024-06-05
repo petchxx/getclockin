36b8b55438989ebabe08781519569cf9d7cf3541
Petchx <petchxpanuphong@gmail.com>
2024-06-06 01:36:50 +0700

    fix: lint

--- src/app/dashboard/employee/page.tsx
+++ src/app/dashboard/employee/page.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import EmployeePage from "~/app/_components/dashboard/employee/EmployeePage";
+import { Employee } from "~/lib/interface/employee";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 
 export default async function page() {
   const employee = await api.employee.getAll();
-  return <EmployeePage employees={employee} />;
+  return <EmployeePage employees={employee as Employee[]} />;
 }
