f8890d6c0ab753687f613e85f484c8ba23e7a70d
Petchx <petchxpanuphong@gmail.com>
2024-06-06 01:34:31 +0700

    fix: lint

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { api } from "~/trpc/server";
 import HomePage from "../_components/app/HomePage";
+import { Employee } from "~/lib/interface/employee";
 
 export default async function page() {
   const employee = await api.employee.get();
   if (!employee) return null;
-  return <HomePage employee={employee} />;
+  return <HomePage employee={employee as Employee} />;
 }
