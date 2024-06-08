3e650b4e3a0571d095d926773bc47020c52a3deb
Petchx <petchxpanuphong@gmail.com>
2024-06-08 17:28:49 +0700

    feat: update border

--- src/app/_components/app/BottomBar.tsx
+++ src/app/_components/app/BottomBar.tsx
@@ -35,7 +35,7 @@ export default function BottomBar() {
     },
   ];
   return (
-    <Card className="fixed bottom-0 flex h-[10%] w-full items-center justify-center rounded-none border-t border-content3 bg-background pb-6 pt-1 shadow-none">
+    <Card className="fixed bottom-0 flex h-[10%] w-full items-center justify-center rounded-none bg-background pb-6 pt-1 shadow-small shadow-content3">
       <div className="flex w-full max-w-screen-sm justify-evenly">
         {items.map((item, index) => (
           <Button
