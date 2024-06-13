4fdd00fe5287f2026f952d4b7c18a92e9857738a
Petchx <petchxpanuphong@gmail.com>
2024-06-13 10:06:17 +0700

    feat: update settings page

--- src/app/_components/dashboard/SettingsPage.tsx
+++ src/app/_components/dashboard/SettingsPage.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { Button, Card, Divider, Input } from "@nextui-org/react";
 import React, { useState } from "react";
+import { toast } from "react-toastify";
 import { type Company } from "~/lib/interface/company";
+import { api } from "~/trpc/react";
 
 type Props = {
   company: Company;
@@ -12,6 +14,22 @@ export default function SettingsPage({ company }: Props) {
   const [email, setEmail] = useState(company.email ?? "");
   const [lineToken, setLineToken] = useState(company.line_token ?? "");
   const [appPassword, setAppPassword] = useState(company.app_password ?? "");
+
+  const updateCompany = api.company.update.useMutation({
+    async onSuccess() {
+      toast.success("บันทึกสำเร็จ");
+    },
+  });
+
+  async function handleSubmit() {
+    updateCompany.mutate({
+      name,
+      email,
+      line_token: lineToken,
+      app_password: appPassword,
+    });
+  }
+
   return (
     <div className="py-4">
       <Card className="">
@@ -22,12 +40,20 @@ export default function SettingsPage({ company }: Props) {
             className="mt-4"
             variant="bordered"
             defaultValue={email}
+            onChange={(e) => setEmail(e.target.value)}
             name="email"
           />
         </div>
 
         <Divider className="" />
-        <Button className="m-4 w-24" variant="flat" color="primary">
+        <Button
+          onClick={async () => {
+            await handleSubmit();
+          }}
+          className="m-4 w-24"
+          variant="flat"
+          color="primary"
+        >
           บันทึก
         </Button>
       </Card>
@@ -35,11 +61,25 @@ export default function SettingsPage({ company }: Props) {
         <div className="m-4">
           <p className="text font-bold">ชื่อบริษัท</p>
           <p className="text-sm text-foreground/50">ใช้ในการเข้าแอปพลิเคชั่น</p>
-          <Input className="mt-4" variant="bordered" defaultValue={name} />
+          <Input
+            className="mt-4"
+            variant="bordered"
+            defaultValue={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
         </div>
 
         <Divider className="" />
-        <Button className="m-4 w-24" variant="flat" color="primary">
+        <Button
+          className="m-4 w-24"
+          onClick={async () => {
+            await handleSubmit();
+          }}
+          variant="flat"
+          color="primary"
+        >
           บันทึก
         </Button>
       </Card>
@@ -49,11 +89,23 @@ export default function SettingsPage({ company }: Props) {
           <p className="text-sm text-foreground/50">
             ใช้ในการแจ้งเตือนผ่านไลน์
           </p>
-          <Input className="mt-4" variant="bordered" defaultValue={lineToken} />
+          <Input
+            className="mt-4"
+            variant="bordered"
+            onChange={(e) => {
+              setLineToken(e.target.value);
+            }}
+            defaultValue={lineToken}
+          />
         </div>
 
         <Divider className="" />
-        <Button className="m-4 w-24" variant="flat" color="primary">
+        <Button
+          onClick={() => handleSubmit()}
+          className="m-4 w-24"
+          variant="flat"
+          color="primary"
+        >
           บันทึก
         </Button>
       </Card>
@@ -94,6 +146,9 @@ export default function SettingsPage({ company }: Props) {
             className="m-4 w-24"
             variant="flat"
             color="primary"
+            onClick={async (e) => {
+              await handleSubmit();
+            }}
           >
             บันทึก
           </Button>
@@ -108,11 +163,21 @@ export default function SettingsPage({ company }: Props) {
             variant="bordered"
             type="password"
             defaultValue={company.app_password ?? ""}
+            onChange={(e) => {
+              setAppPassword(e.target.value);
+            }}
           />
         </div>
 
         <Divider className="" />
-        <Button className="m-4 w-24" variant="flat" color="primary">
+        <Button
+          className="m-4 w-24"
+          onClick={async () => {
+            await handleSubmit();
+          }}
+          variant="flat"
+          color="primary"
+        >
           บันทึก
         </Button>
       </Card>
